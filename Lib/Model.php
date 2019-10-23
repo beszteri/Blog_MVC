e49027a5afa3ed668ea239003389ee842bf96707
Lajos Beszteri <44468445+beszteri@users.noreply.github.com>
2019-10-23 13:03:56 +0200

    Models

---
+++ Lib/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+class Model {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+}
---
+++ Model/User.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class User {
+
+    private $id;
+    private $name;
+    private $password;
+
+
+    public function __construct($id, $name, $password)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->password = $password;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
