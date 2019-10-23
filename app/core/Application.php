8e012ea2934f3f270b6e17bba7a6cd26befc7d92
Lajos Beszteri <44468445+beszteri@users.noreply.github.com>
2019-10-23 19:37:29 +0200

    rethink the whole proj

---
+++ app/core/Application.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class Application
+{
+
+    protected $controller = 'homeController';
+    protected $action = 'index';
+    protected $prams = [];
+
+    /**
+     * Application constructor.
+     */
+    public function __construct()
+    {
+        $this->prepareURL();
+        if(file_exists(CONTROLLER. $this->controller . '.php')){
+            $this->controller = new $this->controller;
+            if(method_exists($this->controller, $this->action)){
+                call_user_func_array([$this->controller, $this->action], $this->prams);
+            }
+        }
+
+    }
+
+    protected function prepareURL(){
+        $request = trim($_SERVER['REQUEST_URI'], '/');
+        if(!empty($request)){
+            $url = explode( '/', $request);
+            $this->controller = isset($url[0]) ? $url[0] . 'Controller' : 'homeController';
+            $this->action = isset($url[1]) ? $url[1] : 'index';
+            unset($url[0], $url[1]);
+            $this->prams = !empty($url) ? array_values($url) : [];
+        }
+    }
+
+}
---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+define('ROOT', dirname(__DIR__) . 'DIRECTORY_SEPARATOR');
+define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
+define('VIEW', ROOT . 'app' . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
+define('MODEL', ROOT . 'app' . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR);
+define('DATA', ROOT . 'app' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR);
+define('CORE', ROOT . 'app' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR);
+define('CONTROLLER', ROOT . 'app' . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR);
+
+$modules = [ROOT, APP, CORE, CONTROLLER, DATA];
+
+set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules));
+spl_autoload_register('spl_autoload', false);
+
+new Application();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+asddddddddddd
+</body>
+</html>
